0a9e4b719705fb28a51bd4b9611b5d7bcf191c9c
Gustavo Brian <gustavo.brian@sembo.se>
2018-05-19 06:19:59 +0200

    basic service to test heroku

---
+++ src/service/accommodation/image/service.js
@@ -0,0 +1,13 @@
+function AccommodationImageService(){
+  this.URL_BASE = '/accommmodation/image';
+};
+AccommodationImageService.prototype.apiRoute = function(){
+  return this.URL_BASE;
+}
+AccommodationImageService.prototype.get_Classify = function(req, res) {
+  res.json({ message: `Classify service`});   
+};
+AccommodationImageService.prototype.post_Classify = function(req, res) {
+  res.json({ message: `Classify service`});   
+};
+module.exports = AccommodationImageService;
